9b4a318ef1a56717edd3291dd8837af96b318649
cfree14 <cfree14@gmail.com>
2023-05-16 13:22:45 -0400

    insurance work

---
+++ code/Step1_build_ram_data.R
@@ -0,0 +1,64 @@
+
+
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(tidyverse)
+
+# Directories
+plotdir <- "figures"
+datadir <- "data"
+
+# Read RAM data
+load("/Users/cfree/Dropbox/Chris/UCSB/data/ramldb/RAMLDB v4.495/R Data/DBdata[asmt][v4.495].RData")
+
+# Read SST data
+sst_orig <- read.csv("/Users/cfree/Dropbox/Chris/UCSB/projects/wc_cc_synthesis/data/environmental/cobe/processed/COBE_1981_2020_by_ram_stocks.csv")
+
+
+# Format RAM data
+################################################################################
+
+# Stock key
+stock_key <- stock %>%
+  # West Coast
+  filter(region=="US West Coast") %>%
+  # Add area
+  left_join(area %>% select(areaid, areaname), by="areaid") %>%
+  # Simplify
+  select(stockid, scientificname, commonname, stocklong, areaname) %>%
+  # Rename
+  rename(comm_name=commonname, sci_name=scientificname, area=areaname)
+
+# Stock assessment data
+data <- timeseries_values_views %>%
+  # West Coast
+  filter(stockid %in% stock_key$stockid) %>%
+  # Simplify
+  select(stockid, year, TB, SSB, TN) %>%
+  # Deciding to use TB
+  select(stockid, year, TB) %>%
+  na.omit() %>%
+  # Rename
+  rename(biomass=TB) %>%
+  # Add species info
+  left_join(stock_key %>% select(stockid, stocklong, comm_name, sci_name, area), by="stockid") %>%
+  # Add SST data
+  left_join(sst_orig, by=c("stockid", "year")) %>%
+  # Arrange
+  select(stockid, stocklong, comm_name, sci_name, area, year, biomass, everything())
+
+# Inspect
+freeR::complete(data)
+
+# Export data
+saveRDS(data, file=file.path(datadir, "WC_RAM_data_processed.Rds"))
+
+
+
+
---
+++ code/Step2_add_pacfin_data.R
@@ -0,0 +1,87 @@
+
+
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(tidyverse)
+
+# Directories
+plotdir <- "figures"
+datadir <- "data"
+
+# Get PACFIN data
+pacfin_orig <- wcfish::pacfin_all1
+
+# Read RAM data
+ram_orig <- readRDS(file.path(datadir, "WC_RAM_data_processed.Rds"))
+
+
+# Format PACFIN data
+################################################################################
+
+# Summarize
+pacfin <- pacfin_orig %>%
+  # Reduce to species
+  filter(!is.na(sci_name)) %>%
+  # Summarize by species-year
+  group_by(comm_name, sci_name, year) %>%
+  summarize(value_usd=sum(value_usd, na.rm = T),
+            landings_lb=sum(landings_lb, na.rm=T)) %>%
+  ungroup() %>%
+  # Calculate ex-vessel price
+  mutate(price_usd_lb=value_usd/landings_lb)
+
+# Check names
+freeR::check_names(pacfin$sci_name)
+
+
+# Format RAM data
+################################################################################
+
+# Format
+ram <- ram_orig %>%
+  # Record number of stocks
+  group_by(comm_name, sci_name) %>%
+  mutate(nstocks=n_distinct(stockid)) %>%
+  ungroup() %>%
+  # Summarize
+  group_by(comm_name, sci_name, year, nstocks) %>%
+  summarize(nstocks_w_data=n_distinct(stockid),
+            biomass=sum(biomass),
+            sst_c_avg=mean(sst_c_avg)) %>%
+  ungroup() %>%
+  # Reduce to years with biomass estimates for all stocks
+  filter(nstocks==nstocks_w_data) %>%
+  select(-nstocks_w_data)
+
+# Check names
+freeR::check_names(ram$sci_name)
+
+
+# Merge data
+################################################################################
+
+# Merge data
+data <- ram %>%
+  # Add PACFIN revenues and price
+  left_join(pacfin %>% select(sci_name, year, value_usd, price_usd_lb), by=c("sci_name", "year")) %>%
+  # Reduce to years with all data
+  na.omit() %>%
+  # Arrange
+  arrange(comm_name, year)
+
+
+# Export data
+################################################################################
+
+# Export data
+saveRDS(data, file=file.path(datadir, "west_coast_fish_stock_data.Rds"))
+write.csv(data, file=file.path(datadir, "west_coast_fish_stock_data.csv"), row.names=F)
+
+
+
